23dffd65a25a2f6dec90a1b86a0195369599543b
bramex <32403451+bramex@users.noreply.github.com>
2023-07-27 19:39:56 -0600

    Clase 6 Completa

--- app/src/main/java/com/bramex/enfermericclient/MainActivity.kt
+++ app/src/main/java/com/bramex/enfermericclient/MainActivity.kt
@@ -2,10 +2,12 @@ package com.bramex.enfermericclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
     }
 }
